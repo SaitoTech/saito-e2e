571ed120e5028f331226264b6497c3e1a92287cb
davikstone <onyeji@saito.tech>
2024-01-09 01:55:47 +0100

    update deploy script

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig, devices} from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -24,7 +24,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://127.0.0.1:12101',
+        baseURL: 'http://localhost:12101',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -34,17 +34,17 @@ export default defineConfig({
     projects: [
         {
             name: 'chromium',
-            use: {...devices['Desktop Chrome']},
+            use: { ...devices['Desktop Chrome'] },
         },
 
         {
             name: 'firefox',
-            use: {...devices['Desktop Firefox']},
+            use: { ...devices['Desktop Firefox'] },
         },
 
         {
             name: 'webkit',
-            use: {...devices['Desktop Safari']},
+            use: { ...devices['Desktop Safari'] },
         },
 
         /* Test against mobile viewports. */
