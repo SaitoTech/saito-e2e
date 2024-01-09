ca19122121a6e52872dc348fd2dda7a9e45e920a
davikstone <onyeji@saito.tech>
2024-01-09 13:37:53 +0100

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        baseURL: 'http://localhost:12101', // Adjust if your server URL is different
+        baseURL: 'http://localhost:12101',
         trace: 'on-first-retry',
         headless: true
     },
