5c729288a04c8bb6b08dbf9716f1700dd073fad4
davikstone <onyeji@saito.tech>
2024-01-17 13:29:47 +0100

    add redsquare mod

--- src/mods/module_page.ts
+++ src/mods/module_page.ts
@@ -11,7 +11,7 @@ export default class ModulePage {
 
     async goto() {
         await this.page.goto(this.url);
-        // TODO : check if this timeout can be removed and start testing when the page is loade
+        // TODO : check if this timeout can be removed and start testing when the page is loaded
         await this.page.waitForTimeout(1000);
 
         // await this.page.waitForLoadState();
