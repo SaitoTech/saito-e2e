6537d11b9c0c024561fe5a5553aaac6ba2fac8e8
davikstone <onyeji@saito.tech>
2024-01-17 13:46:35 +0100

    update redsquare specs

--- src/mods/redsquare/redsquare_page.ts
+++ src/mods/redsquare/redsquare_page.ts
@@ -1,7 +1,7 @@
 import { type Page, type Locator, expect } from '@playwright/test'
 import ModulePage from "../module_page";
 
-export default class ArcadePage extends ModulePage {
+export default class RedsquarePage extends ModulePage {
 
     constructor(page: Page) {
         super(page, "/redsquare");
@@ -11,16 +11,24 @@ export default class ArcadePage extends ModulePage {
 
     }
 
-    async createChessGame() {
-        let chessMenuItem = this.page.locator("#Chess");
-        await chessMenuItem.waitFor();
-        expect(chessMenuItem).toBeTruthy();
-        await chessMenuItem.click();
+    async createNewTweet() {
+        const newTweetButton = this.page.locator('#new-tweet');
+        await newTweetButton.click()
+        const tweetTextArea = this.page.locator('#post-tweet-textarea');
+        await expect(tweetTextArea).toBeVisible();
 
-        let createGameButton = this.page.locator(".saito-multi-select_btn_options", { hasText: "create public invite" });
-        await chessMenuItem.waitFor();
-        expect(createGameButton).toBeTruthy();
-        await createGameButton.click();
+        const tweetText = "Hello, this is a test tweet!";
+        await tweetTextArea.fill(tweetText);
+
+        const postTweetButton = this.page.locator('#post-tweet-button');
+        await postTweetButton.click();
+
+
+        await this.page.waitForTimeout(5000);
+
+        const tweetTextElement = this.page.locator('.tweet-text', { hasText: tweetText });
+
+        await expect(tweetTextElement).toBeVisible();
     }
 }
 
