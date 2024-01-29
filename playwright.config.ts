99735cccf1754ebaed8fc716493e7fd5972c7f44
davikstone <onyeji@saito.tech>
2024-01-29 23:57:51 +0100

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,10 +27,5 @@ export default defineConfig({
         },
     ],
     timeout: 30000,
-    // Uncomment and adjust if you need to start a server as part of your tests
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://localhost:12101', // Adjust to match your server
-    //   reuseExistingServer: !process.env.CI,
-    // },
+
 });
